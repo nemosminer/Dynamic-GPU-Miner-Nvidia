299a329f036151272972080ca5076b8efcd382d8
nemosminer <slbaker@hotmail.co.nz>
2019-01-22 18:33:37 +1300

    Revert "Update ccminer-config.h"
    This reverts commit cc4f3492a59eaccaeb788b69cd8e416e2932bdb7.

--- compat/ccminer-config.h
+++ compat/ccminer-config.h
@@ -152,19 +152,19 @@
 /* #undef NO_MINUS_C_MINUS_O */
 
 /* Name of package */
-#define PACKAGE "ccminer-dyn-rtx"
+#define PACKAGE "ccminer-dyn"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "ccminer-dyn-rtx"
+#define PACKAGE_NAME "ccminer-dyn"
 
 /* Define to the home page for this package. */
-#define PACKAGE_URL "https://github.com/nemosminer/Dynamic-GPU-Miner-Nvidia"
+#define PACKAGE_URL "https://github.com/duality-solutions/Dynamic-GPU-Miner-Nvidia"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.1"
+#define PACKAGE_VERSION "1.0.0"
 
 /* If using the C implementation of alloca, define if you know the
    direction of stack growth for your system; otherwise it will be
